c08666349080a98aae0dd4156dfca720f3c990b4
Gagarin Rout <gagarinrout@gmail.com>
2022-12-12 23:13:20 +0530

    modified for render hosting

--- app.js
+++ app.js
@@ -57,8 +57,10 @@ app.post("/failure", function(req1, res1){
     res1.redirect("/");
 })
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is running on port 3000!");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+    console.log("Server is running on port " + PORT + ".");
 });
 
 
